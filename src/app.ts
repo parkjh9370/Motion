d4693bbc0ec324915aed4fb8c253148b734da594
Jaehyeon <parkjh9370@naver.com>
2022-06-02 13:56:04 +0900

    item Add, style css

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,80 @@
-import { Component } from "./components/component.js";
-import { InputDialog } from "./components/dialog/dialog.js";
-import { ImageComponent } from "./components/page/item/image.js";
-import { NoteComponent } from "./components/page/item/note.js";
-import { TodoComponent } from "./components/page/item/todo.js";
+import { TextSectionInput } from "./components/dialog/input/text-input.js";
+import { MediaSectionInput } from "./components/dialog/input/media-input.js";
+import {
+  InputDialog,
+  MediaData,
+  TextData,
+} from "./components/dialog/dialog.js";
 import { VideoComponent } from "./components/page/item/video.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { ImageComponent } from "./components/page/item/image.js";
 import {
   Composable,
   PageComponent,
   PageItemComponent,
 } from "./components/page/page.js";
+import { Component } from "./components/component.js";
+
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  new (): T;
+};
 
 class App {
   private readonly page: Component & Composable;
-  constructor(appRoot: HTMLElement) {
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent(
-      "Image Title",
-      "https://picsum.photos/600/300"
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-image",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
     );
-    this.page.addChild(image);
 
-    const video = new VideoComponent(
-      "Video Title",
-      "https://www.youtube.com/embed/gd4cwivKWfM"
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-video",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
     );
-    this.page.addChild(video);
 
-    const note = new NoteComponent("Note title", "Note Body");
-    this.page.addChild(note);
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-note",
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
 
-    const todo = new TodoComponent("Todo Title", "Todo Item");
-    this.page.addChild(todo);
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-todo",
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
+  }
 
-    const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
-    imageBtn.addEventListener("click", () => {
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    selector: string,
+    InputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const element = document.querySelector(selector)! as HTMLButtonElement;
+    element.addEventListener("click", () => {
       const dialog = new InputDialog();
+      const input = new InputComponent();
+      dialog.addChild(input);
+      dialog.attachTo(this.dialogRoot);
 
       dialog.setOnCloseListener(() => {
-        dialog.removeFrom(document.body);
+        dialog.removeFrom(this.dialogRoot);
       });
       dialog.setOnSubmitListener(() => {
-        dialog.removeFrom(document.body);
+        const image = makeSection(input);
+        this.page.addChild(image);
+        dialog.removeFrom(this.dialogRoot);
       });
-
-      dialog.attachTo(document.body);
     });
   }
 }
 
-// type assersion, 해당 document는 HTML 파일에서 정의 해두었기 때문에 설정 가능
-new App(document.querySelector(".document")! as HTMLElement);
+new App(document.querySelector(".document")! as HTMLElement, document.body);
+
+// https://picsum.photos/600/300
